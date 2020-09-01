b9261a1927599bc124770dcdbc7addca86b86a7f
Eduardo M Gomes <eduardo.2001.06@gmail.com>
2020-09-01 01:01:45 -0300

    Remove Key/email authentication add better exception when can't find the record id

--- cf.py
+++ cf.py
@@ -8,40 +8,33 @@ import configparser
 APIURL="https://api.cloudflare.com/client/v4/"
 
 class authObj:
-	APIKey = ""
-	APIEmail = ""
-	APIToken = ""
-	AuthType = 0
+	APIKey = None
+	APIEmail = None
+	APIToken = None
 	def getAuthHeader(self):
 		header = dict()
 		header["Content-Type"] = "application/json"
-		if self.AuthType&1: #APIToken
+		if self.APIToken is not None: #APIToken
 			header["Authorization"] = "Bearer " + self.APIToken
-		elif self.AuthType&2: #APIKey
-			header["X-Auth-Key"] = self.APIKey
-			header["X-Auth-Email"] = self.APIEmail
 		else:
 			print("Authentication not defined")
-			raise RuntimeError
+			raise RuntimeError("Authentication not defined")
 		return header
 	def __init__(self, configAuth):
-		self.AuthType = checkConfigAuthIsSet(configAuth)
-		self.APIKey = configAuth.get('APIKey', '')
-		self.APIEmail = configAuth.get('APIEmail', '')
 		self.APIToken = configAuth.get('APIToken', '')
 
 class address:
-	ipv4 = ""
-	ipv6 = ""
+	ipv4 = None
+	ipv6 = None
 
 
 class Record:
 	A = False
 	AAAA = False
-	name = ""
-	Id = ""
-	Idv6 = ""
-	ZoneId = ""
+	name = None
+	Id = None
+	Idv6 = None
+	ZoneId = None
 	ttl = 120
 	def __init__(self, record_conf:configparser.SectionProxy):
 		self.A = record_conf.getboolean('A', False)
@@ -74,18 +67,27 @@ class Zone:
 			recordv4 = requests.get(URL + "&type=A", headers=headers)
 			recordv4 = json.loads(recordv4.text)
 			if not int(recordv4.get("result_info").get("count")):
-				raise RuntimeError("Record not found")
+				raise RecordNotFoundInZone(self, record)
 			record.Id = recordv4.get("result")[0].get("id")
 		if record.AAAA:
-			print(URL)
 			recordv6 = requests.get(URL + "&type=AAAA", headers=headers)
 			recordv6 = json.loads(recordv6.text)
 			if not int(recordv6.get("result_info").get("count")):
-				raise RuntimeError("Record not found")
+				raise RecordNotFoundInZone(self, record)
 			record.Idv6 = recordv6.get("result")[0].get("id")
 		record.ZoneId = self.Id
 		print(record.name, record.Id, record.Idv6)
 
+class RecordNotFoundInZone(Exception):
+	def __init__(self, zone:Zone, record:Record):
+		self.ZoneName = zone.Name
+		self.ZoneId = zone.Id
+		self.RecordName = record.name
+		self.RecordId = record.Id
+		self.RecordIdv6 = record.Idv6
+	
+	def __str__(self):
+		return "Couldn't find record (" + self.RecordName + ") at zone (" + self.ZoneName + ")\ncheck if it exists"
 
 
 def interactiveConfig(config_obj):
@@ -127,18 +129,6 @@ def checkConfig(config_obj):
 		raise KeyError
 
 #%%
-print("Try to get ip")
-try:
-	print("Try to get ipv4")
-	address.ipv4 = requests.get('https://api.ipify.org', timeout=15).text
-except:
-	print("Can't get ipv4")
-try:
-	print("Try to get ipv6")
-	address.ipv6 = requests.get('https://api6.ipify.org', timeout=15).text
-except:
-	print("Can't get ipv6")
-#%%
 config = configparser.ConfigParser()
 try:
 	with open('conf.ini') as file:
@@ -156,6 +146,19 @@ auth = config['AUTH']
 with open('conf.ini', 'w') as configfile:
    config.write(configfile)
 authHeader = authObj(auth)
+
+#%%
+print("Try to get ip")
+try:
+	address.ipv4 = requests.get('https://api.ipify.org', timeout=15).text
+	print("Got ipv4: " + address.ipv4)
+except:
+	print("Can't get ipv4")
+try:
+	address.ipv6 = requests.get('https://api6.ipify.org', timeout=15).text
+	print("Got ipv6: " + address.ipv6)
+except:
+	print("Can't get ipv6")
 
 ## record config
 
